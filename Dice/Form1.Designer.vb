f5f20608245b3b3e181e3cc811d515eb95ddae6a
Bugatti1982 <59714111+Bugatti1982@users.noreply.github.com>
2020-11-15 16:35:01 -0600

    Initial commit

---
+++ Dice/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnRoll = New System.Windows.Forms.Button()
+        Me.MyPicBox = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.MyPicBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnRoll
+        '
+        Me.btnRoll.Location = New System.Drawing.Point(235, 321)
+        Me.btnRoll.Name = "btnRoll"
+        Me.btnRoll.Size = New System.Drawing.Size(75, 23)
+        Me.btnRoll.TabIndex = 0
+        Me.btnRoll.Text = "Roll"
+        Me.btnRoll.UseVisualStyleBackColor = True
+        '
+        'MyPicBox
+        '
+        Me.MyPicBox.Location = New System.Drawing.Point(84, 36)
+        Me.MyPicBox.Name = "MyPicBox"
+        Me.MyPicBox.Size = New System.Drawing.Size(366, 259)
+        Me.MyPicBox.TabIndex = 1
+        Me.MyPicBox.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(547, 411)
+        Me.Controls.Add(Me.MyPicBox)
+        Me.Controls.Add(Me.btnRoll)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.MyPicBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnRoll As Button
+    Friend WithEvents MyPicBox As PictureBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Dice/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim t As Integer
+        t = 0
+        Do
+
+            MyPicBox.Refresh()
+
+            Dim n As Integer
+            Dim myPen As Pen
+
+            myPen = New Pen(Drawing.Color.DarkTurquoise, 10)
+            Dim myGraphics As Graphics = MyPicBox.CreateGraphics
+            n = Int(6 * Rnd()) + 1
+
+            Select Case n
+
+                Case 1
+                    myGraphics.DrawEllipse(myPen, 80, 80, 10, 10)
+
+                Case 2
+                    myGraphics.DrawEllipse(myPen, 40, 40, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 120, 120, 10, 10)
+
+                Case 3
+                    myGraphics.DrawEllipse(myPen, 40, 40, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 80, 80, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 120, 120, 10, 10)
+
+                Case 4
+                    myGraphics.DrawEllipse(myPen, 40, 40, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 120, 40, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 40, 120, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 120, 120, 10, 10)
+                Case 5
+                    myGraphics.DrawEllipse(myPen, 40, 40, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 120, 40, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 80, 80, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 40, 120, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 120, 120, 10, 10)
+
+                Case 6
+                    myGraphics.DrawEllipse(myPen, 40, 40, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 120, 40, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 40, 80, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 120, 80, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 40, 120, 10, 10)
+                    myGraphics.DrawEllipse(myPen, 120, 120, 10, 10)
+            End Select
+            t = t + 1
+        Loop Until t > 1000
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub btnRoll_Click(sender As Object, e As EventArgs) Handles btnRoll.Click
+        Timer1.Enabled = True
+    End Sub
+End Class
